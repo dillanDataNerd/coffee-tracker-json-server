a09be302d8cda42ee499c9854285114397963a48
dillandatanerd <dillanpatelnz@gmail.com>
2025-08-27 11:56:20 +0200

    fix: added env variables

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ try{
     console.log(error)
 }
 const jsonServer = require('json-server');
-const middlewares=jsonServer.defaults()
-
 const server = jsonServer.create() 
 const router=jsonServer.router("db.json")
+const middlewares=jsonServer.defaults()
+
+const PORT = process.env.PORT 
 
 server.use(middlewares)
 server.use(router)
-const PORT = process.env.PORT 
 
 server.listen(PORT, () => {
   console.log(`JSON server is running at port ${PORT}`)
   console.log(`Local Access at http://localhost:${PORT}`)
-})
+})ß
